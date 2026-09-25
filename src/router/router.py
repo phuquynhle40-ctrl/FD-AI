fbc21e757ba74b992a0f99ce330194511ca200b7
phuquynhle40-ctrl <phuquynhle40@gmail.com>
2026-09-25 09:34:56 +0700

    Update router.py

--- src/router/router.py
+++ src/router/router.py
@@ -1,1 +1,13 @@
+class Router:
+    def __init__(self, tool_manager, module_manager, version_manager):
+        self.tool_manager = tool_manager
+        self.module_manager = module_manager
+        self.version_manager = version_manager
 
+    def handle(self, message):
+        return {
+            "message": message,
+            "version": self.version_manager.current_version(),
+            "tools": self.tool_manager.list_tools(),
+            "modules": self.module_manager.list_modules(),
+        }
